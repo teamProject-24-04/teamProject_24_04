f23255fe1d1f92dbd88bf58248723f67b0d8b2db
dhckdwls <codetama51@gmail.com>
2024-04-16 17:39:41 +0900

    연동 테스트 2

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         안녕하세요 새테스트1
+        반갑습니다 새테스트2
       </div>
     </main>
   );
