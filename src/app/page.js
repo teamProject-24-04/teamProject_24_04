54767a4c70b86405642ac9e26c39357dabec3813
dhckdwls <codetama51@gmail.com>
2024-04-16 17:40:57 +0900

    연동 테스트 3

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         안녕하세요 새테스트1
         반갑습니다 새테스트2
+        네 새테스트3
       </div>
     </main>
   );
