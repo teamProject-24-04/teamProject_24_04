6905e67dd833da9a99632c0172b25aae74c0d902
dhckdwls <codetama51@gmail.com>
2024-04-17 10:14:57 +0900

    테스트2

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         안녕하세요 새테스트12 안녕하세요 새테스트1 반갑습니다 새테스트2 네 새테스트3 테스트 오창진
-        테스트테스트
-        안녕하세요 새테스트1
-        반갑습니다 새테스트2
-        네 새테스트3n
+        테스트테스트 qweqweqweqw qweqweqweq w
       </div>
     </main>
   );
