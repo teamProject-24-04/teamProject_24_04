0273be691018352df508452694f1cb0193f712fa
pshpark7 <pshpark07@gmail.com>
2024-04-19 17:37:34 +0900

    MyPage 추가중

--- src/app/mypage/page.js
+++ src/app/mypage/page.js
@@ -14,10 +14,11 @@ import {
   Tyopgraphy,
 } from '@mui/material';
 import { IoPersonAdd, IoSettingsOutline } from "react-icons/io5";
-import { CgProfile } from "react-icons/cg";
 import { LuPencil } from "react-icons/lu";
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { LuBookCopy } from "react-icons/lu";
+import { FaPlus } from "react-icons/fa";
 
 export default function Sub() {
   useEffect(() => {
@@ -43,23 +44,14 @@ export default function Sub() {
 const card = (
   <React.Fragment>
     <CardContent style={{backgroundColor:'#ACB9D1'}}>
-      <Typography sx={{ fontSize: 14, fontWeight: 'bold' }} color="text.secondary" gutterBottom>
+    <Typography sx={{ fontSize: 14, fontWeight: 'bold', display: 'flex', alignItems: 'center' }} color="text.secondary" gutterBottom>   
         나의 레시피
+        <FaPlus style={{ marginLeft: '75px' }} />
       </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
+      <LuBookCopy style={{ fontSize: '60px' }}/>
     </CardContent>
     <CardActions>
-      <Button size="small">Learn More</Button>
+      <Button size="small">기록 하기</Button>
     </CardActions>
   </React.Fragment>
 );
@@ -69,7 +61,7 @@ const card = (
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>   
         <IoPersonAdd style={{ marginRight: '12px', fontSize: '24px' }} />
         <IoSettingsOutline style={{ fontSize: '24px' }} />
-      </div>
+      </div>                                                                                                                                                                           
       <div style={{ display: 'flex', alignItems: 'center', marginTop: '100px', marginLeft: '30px' }}>
       <Stack direction="row" spacing={2}>
       <Avatar  alt="Remy Sharp" src="/profile/cat.jpg" />
@@ -77,8 +69,40 @@ const card = (
         <div style={{ fontWeight: 'bold',marginLeft: '13px' }}>김선우</div>
         <LuPencil style={{ marginLeft: '13px',marginTop:'7px', fontSize:'9px'}}/>
       </div>
-      <Box sx={{ width: '50%', marginTop:'20px' }}>
+      <Box sx={{ display: 'flex', marginTop: '20px' }}>
   <Card variant="outlined">{card}</Card>
+  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+    <Box
+      marginLeft={1}
+      height={50}
+      width={150}
+      backgroundColor='#ACB9D1'
+      display="flex"
+      alignItems="center"
+      gap={4}
+      p={2}
+      sx={{ border: '2px solid grey' }}
+    >
+      가입 날짜
+      2024.04.15
+    </Box>
+    <Box
+      marginLeft={1}
+      height={50}
+      width={150}
+      backgroundColor='#ACB9D1'
+      my={1}
+      display="flex"
+      alignItems="center"
+      gap={4}
+      p={2}
+      sx={{ border: '2px solid grey' }}
+    >
+      레시피 수
+      <br></br>
+      5개
+    </Box>
+  </Box>
 </Box>
     </>
   );
