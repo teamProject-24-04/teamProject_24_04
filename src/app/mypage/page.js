70acac5ca57a60aa8b98b92b750ec5b0780ffab7
pshpark7 <pshpark07@gmail.com>
2024-04-22 11:16:46 +0900

    MyPage 추가 및 메뉴바 미적용

--- src/app/mypage/page.js
+++ src/app/mypage/page.js
@@ -19,6 +19,8 @@ import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import { LuBookCopy } from "react-icons/lu";
 import { FaPlus } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { IoIosArrowForward } from "react-icons/io";
 
 export default function Sub() {
   useEffect(() => {
@@ -46,7 +48,7 @@ const card = (
     <CardContent style={{backgroundColor:'#ACB9D1'}}>
     <Typography sx={{ fontSize: 14, fontWeight: 'bold', display: 'flex', alignItems: 'center' }} color="text.secondary" gutterBottom>   
         나의 레시피
-        <FaPlus style={{ marginLeft: '75px' }} />
+        <FaPlus style={{ marginLeft: '75px', color: '#538DFF' }} />
       </Typography>
       <LuBookCopy style={{ fontSize: '60px' }}/>
     </CardContent>
@@ -62,7 +64,7 @@ const card = (
         <IoPersonAdd style={{ marginRight: '12px', fontSize: '24px' }} />
         <IoSettingsOutline style={{ fontSize: '24px' }} />
       </div>                                                                                                                                                                           
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '100px', marginLeft: '30px' }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginTop: '50px', marginLeft: '30px' }}>
       <Stack direction="row" spacing={2}>
       <Avatar  alt="Remy Sharp" src="/profile/cat.jpg" />
     </Stack>
@@ -81,6 +83,7 @@ const card = (
       alignItems="center"
       gap={4}
       p={2}
+      borderRadius={1}
       sx={{ border: '2px solid grey' }}
     >
       가입 날짜
@@ -96,6 +99,7 @@ const card = (
       alignItems="center"
       gap={4}
       p={2}
+      borderRadius={1}
       sx={{ border: '2px solid grey' }}
     >
       레시피 수
@@ -103,6 +107,92 @@ const card = (
       5개
     </Box>
   </Box>
+</Box>
+<div style={{ display: 'flex' }}> {/* 가로로 나열되는 플렉스 컨테이너 */}
+<Box
+  height={130}
+  width={190}
+  backgroundColor='#ACB9D1'
+  my={4}
+  sx={{ border: '2px solid grey' }}
+  borderRadius={1}
+>
+  <div style={{ marginTop: '7px' }}> {/* 첫 번째 내용과 간격 */}
+    내 게시글 바로가기
+    <FaPlus style={{ marginLeft: '30px', color: '#538DFF' }} />
+  </div>
+  <div style={{ marginTop: '13px', fontWeight: 'bold' }}> {/* 두 번째 내용과 간격 */}
+    35개
+  </div>
+  <div style={{ marginTop: '7px' }}> {/* 세 번째 내용과 간격 */}
+    내 댓글 바로가기
+    <FaPlus style={{ marginLeft: '45px', color: '#538DFF' }} />
+  </div>
+  <div style={{ marginTop: '13px', fontWeight: 'bold' }}> {/* 네 번째 내용과 간격 */}
+    35개
+  </div>
+</Box>
+<Box
+marginLeft={2}
+  height={130}
+  width={190}
+  backgroundColor='#ACB9D1'
+  my={4}
+  sx={{ border: '2px solid grey' }}
+  borderRadius={1}
+>
+  <div style={{ marginTop: '7px' }}> {/* 첫 번째 내용과 간격 */}
+    나의 쇼핑
+    <FaPlus style={{ marginLeft: '95px', color: '#538DFF' }} />
+  </div>
+    <FiShoppingCart style={{ fontSize: '50px', marginTop: '20px', marginLeft:'60px' }} /> {/* 아이콘 크기 조절 및 위치 조정 */}
+  <div style={{ marginTop: '5px', fontWeight: 'bold', textAlign:'right'}}> {/* 네 번째 내용과 간격 */}
+    3개
+  </div>
+</Box>
+</div>
+
+<Box
+  height={130}
+  width={390}
+  backgroundColor='#D9D9D9'
+  sx={{ border: '2px solid grey'}} // 요소 사이 간격 조절
+  borderRadius={1}
+>
+  <div style={{ marginTop: '7px', marginLeft: '7px', display:'flex' }}> {/* 첫 번째 내용과 간격 */}
+    찜한 목록
+    <FaPlus style={{ marginLeft: '290px', color: '#538DFF' }} />
+  </div>
+  <div style={{ marginTop: '20px', marginLeft: '7px', display:'flex' }}> {/* 첫 번째 내용과 간격 */}
+    찜한 유튜버
+    <FaPlus style={{ marginLeft: '274px', color: '#538DFF' }} />
+  </div>
+  <div style={{ marginTop: '20px', marginLeft: '7px', display:'flex' }}> {/* 첫 번째 내용과 간격 */}
+    찜한 친구
+    <FaPlus style={{ marginLeft: '290px', color: '#538DFF' }} />
+  </div>
+</Box>
+
+<Box
+  height={130}
+  width={390}
+  my={2 }
+  backgroundColor='#D9D9D9'
+  sx={{ border: '2px solid grey'}} // 요소 사이 간격 조절
+  borderRadius={1}
+>
+  <div style={{ marginTop: '7px', marginLeft: '7px', display:'flex' }}> {/* 첫 번째 내용과 간격 */}
+    문의 하기
+    <IoIosArrowForward  style={{ marginLeft: '290px', color: '#538DFF' }} />
+  </div>
+  <div style={{ marginTop: '20px', marginLeft: '7px', display:'flex' }}> {/* 첫 번째 내용과 간격 */}
+    비밀번호 변경
+    <IoIosArrowForward  style={{ marginLeft: '258px', color: '#538DFF' }} />
+  </div>
+  <div style={{ marginTop: '20px', marginLeft: '7px', display:'flex' }}> {/* 첫 번째 내용과 간격 */}
+    회원정보 변경
+    <IoIosArrowForward  style={{ marginLeft: '258px', color: '#538DFF' }} />
+  </div>
 </Box>
     </>
   );
