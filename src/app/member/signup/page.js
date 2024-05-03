a598af965ca49d57c0ebe5e591ffb20679543137
ksw70913 <pavico0913@gmail.com>
2024-05-03 10:35:26 +0900

    회원가입에 다음API, 구글API를 활용해 우편,위도,경도 추가

--- src/app/member/signup/page.js
+++ src/app/member/signup/page.js
@@ -1,5 +1,4 @@
 'use client';
-
 import React, { useState, useEffect } from 'react';
 import {
   TextField,
@@ -9,16 +8,16 @@ import {
   List,
   ListItem,
   ListItemText,
+  Snackbar,
 } from '@mui/material';
 import axios from 'axios';
 import * as yup from 'yup';
 
-// Yup 스키마를 사용하여 유효성 검사를 정의합니다.
 const schema = yup.object().shape({
   loginId: yup
     .string()
     .required('아이디를 입력하세요.')
-    .matches(/^[a-zA-Z0-9]*$/, '영어와 숫자만 입력해주세요.'), // 영어와 숫자만 허용하는 정규 표현식
+    .matches(/^[a-zA-Z0-9]*$/, '영어와 숫자만 입력해주세요.'),
   loginPw: yup.string().required('비밀번호를 입력하세요.'),
   confirmLoginPw: yup
     .string()
@@ -26,8 +25,81 @@ const schema = yup.object().shape({
     .oneOf([yup.ref('loginPw'), null], '비밀번호가 일치하지 않습니다.'),
 });
 
+const AddressFinder = ({
+  setAddress,
+  setZonecode,
+  setRoadAddress,
+  setJibunAddress,
+  setLatitude,
+  setLongitude,
+}) => {
+  const id = 'daum-postcode';
+  const src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+
+  useEffect(() => {
+    const loadLayout = () => {
+      window.daum.postcode.load(() => {
+        const postcode = new window.daum.Postcode({
+          oncomplete: async function (data) {
+            console.log(data);
+            setAddress(data.address);
+            setZonecode(data.zonecode); // 우편번호
+            setRoadAddress(data.roadAddress); // 도로명주소
+            setJibunAddress(data.jibunAddress); // 지번주소
+
+            // 구글 지도 API 호출
+            const coords = await getAddressCoords(data.roadAddress);
+            setLatitude(coords.latitude);
+            setLongitude(coords.longitude);
+          },
+        });
+
+        // postcode.open() 호출을 스크립트 로드 후에 실행
+        document.getElementById('openPostcode').addEventListener('click', () => {
+          postcode.open();
+        });
+      });
+    };
+
+    const isAlready = document.getElementById(id);
+
+    if (!isAlready) {
+      const script = document.createElement('script');
+      script.src = src;
+      script.id = id;
+      script.async = true; // 스크립트를 비동기적으로 로드
+      script.onload = loadLayout; // 스크립트 로드 후 loadLayout 함수 실행
+      document.body.append(script);
+    }
+  }, []);
+
+  const getAddressCoords = async (address) => {
+    const API_KEY = 'AIzaSyA_HAeMOsU8-4oVQP80VzX2HUBWrr_LeZ4';
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address,
+    )}&key=${API_KEY}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    const location = data.results[0].geometry.location;
+    const latitude = location.lat;
+    const longitude = location.lng;
+    return { latitude, longitude };
+  };
+
+  return (
+    <div className="App">
+      <Button variant="contained" id="openPostcode">
+        주소찾기
+      </Button>
+    </div>
+  );
+};
+
 export default function Page() {
-  // 상태 변수를 사용하여 아이디, 비밀번호, 비밀번호 확인, 중복 여부, 회원 리스트를 관리합니다.
   const [loginId, setLoginId] = useState('');
   const [loginPw, setLoginPw] = useState('');
   const [confirmLoginPw, setConfirmLoginPw] = useState('');
@@ -38,8 +110,18 @@ export default function Page() {
     loginPw: '',
     confirmLoginPw: '',
   });
+  const [address, setAddress] = useState('');
+  const [zonecode, setZonecode] = useState('');
+  const [roadAddress, setRoadAddress] = useState('');
+  const [jibunAddress, setJibunAddress] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
-  // 회원 리스트를 불러오는 함수를 생성합니다.
+  useEffect(() => {
+    fetchMembers();
+  }, []);
+
   const fetchMembers = async () => {
     try {
       const response = await axios.get('/api/member/members');
@@ -49,16 +131,9 @@ export default function Page() {
     }
   };
 
-  // 컴포넌트가 마운트될 때 한 번만 회원 리스트를 불러옵니다.
-  useEffect(() => {
-    fetchMembers();
-  }, []);
-
-  // 아이디 입력 시 중복 여부를 확인하는 함수
   const handleIdChange = (event) => {
     const newLoginId = event.target.value;
     setLoginId(newLoginId);
-    // 중복 아이디 검사
     const duplicateCheck = members.find((member) => member.loginId === newLoginId);
     if (duplicateCheck) {
       setIsDuplicate(true);
@@ -67,7 +142,6 @@ export default function Page() {
     }
   };
 
-  // 비밀번호 확인 입력 시 일치 여부를 확인하는 함수
   const handleConfirmPasswordChange = (event) => {
     const newPassword = event.target.value;
     setConfirmLoginPw(newPassword);
@@ -78,36 +152,41 @@ export default function Page() {
     }
   };
 
-  // 회원가입 폼을 제출하는 함수를 생성합니다.
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      // 입력값의 유효성을 검사합니다.
       schema
         .validate({ loginId, loginPw, confirmLoginPw }, { abortEarly: false })
         .then(() => {
-          // 중복 아이디 검사
           const duplicateCheck = members.find((member) => member.loginId === loginId);
           if (duplicateCheck) {
             setIsDuplicate(true);
-            return; // 중복 아이디가 있으면 함수 종료
+            return;
           }
-
-          // 중복된 아이디가 없으면 회원가입 처리
           axios
-            .post('/api/member/signup', { loginId, loginPw })
+            .post('/api/member/signup', {
+              loginId,
+              loginPw,
+              zonecode,
+              roadAddress,
+              jibunAddress,
+              latitude,
+              longitude,
+            })
             .then(() => {
-              // 회원가입 후에는 회원 리스트를 다시 불러옵니다.
               fetchMembers();
-              // 회원가입 후 입력 필드 초기화
               setLoginId('');
               setLoginPw('');
-              setConfirmLoginPw('');
+              setZonecode('');
+              setRoadAddress('');
+              setJibunAddress('');
+              setLatitude('');
+              setLongitude('');
+              handleSignupSuccess();
             })
             .catch((error) => console.error('Error writing member:', error));
         })
         .catch((errors) => {
-          // Yup 유효성 검사 오류를 처리합니다.
           errors.inner.forEach((err) => {
             if (err.path === 'loginId') {
               setErrors((prevState) => ({ ...prevState, loginId: err.message }));
@@ -123,8 +202,24 @@ export default function Page() {
     }
   };
 
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
+
+  const handleSnackbarOpen = () => {
+    setSnackbarOpen(true);
+  };
+
+  const handleSignupSuccess = () => {
+    handleSnackbarOpen();
+    // 회원가입 성공 후 페이지 이동
+    window.location.href = '/member/mypage'; // 페이지 이동
+  };
+
   return (
-    // MUI 컨테이너로 UI를 감싸고, 회원가입 폼과 회원 리스트를 표시합니다.
     <Container maxWidth="md">
       <div>
         <form onSubmit={handleSubmit} style={{ marginBottom: '20px' }}>
@@ -189,6 +284,51 @@ export default function Page() {
               }
             }}
           />
+          <AddressFinder
+            setAddress={setAddress}
+            setZonecode={setZonecode}
+            setRoadAddress={setRoadAddress}
+            setJibunAddress={setJibunAddress}
+            setLatitude={setLatitude}
+            setLongitude={setLongitude}
+          />
+          <TextField
+            variant="outlined"
+            label="우편번호"
+            value={zonecode}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            variant="outlined"
+            label="도로명주소"
+            value={roadAddress}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            variant="outlined"
+            label="지번주소"
+            value={jibunAddress}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            variant="outlined"
+            label="위도"
+            value={latitude} // 위도 상태 표시
+            fullWidth
+            margin="normal"
+            style={{ display: 'none' }}
+          />
+          <TextField
+            variant="outlined"
+            label="경도"
+            value={longitude} // 경도 상태 표시
+            fullWidth
+            margin="normal"
+            style={{ display: 'none' }}
+          />
           <Button
             variant="outlined"
             type="submit"
@@ -200,18 +340,12 @@ export default function Page() {
           </Button>
         </form>
       </div>
-      <div>
-        <Typography variant="h4" gutterBottom>
-          Member
-        </Typography>
-        <List>
-          {members.map((member) => (
-            <ListItem key={member.id}>
-              <ListItemText primary={member.loginId} secondary={member.loginPw} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        message="회원가입이 성공했습니다."
+      />
     </Container>
   );
 }
--- src/pages/api/member/signup.js
+++ src/pages/api/member/signup.js
@@ -1,21 +1,20 @@
-// src/pages/api/articleWrite.js
 import pool from '../../../app/lib/db';
 
 export default async function handler(req, res) {
   try {
-    // 요청에서 제목과 내용 값을 가져옴
-    const { loginId, loginPw } = req.body;
+    // 요청에서 로그인 아이디, 비밀번호, 우편번호, 도로명주소, 지번주소, 위도, 경도 값을 가져옴
+    const { loginId, loginPw, zonecode, roadAddress, jibunAddress, latitude, longitude } = req.body;
 
-    // 제목과 내용이 없는 경우 에러 반환
+    // 로그인 아이디와 비밀번호가 없는 경우 에러 반환
     if (!loginId || !loginPw) {
       return res.status(400).json({ error: 'loginId and loginPw are required' });
     }
 
-    // article 테이블에 데이터 추가
-    const [result] = await pool.execute('INSERT INTO member (loginId, loginPw) VALUES (?, ?)', [
-      loginId,
-      loginPw,
-    ]);
+    // member 테이블에 데이터 추가
+    const [result] = await pool.execute(
+      'INSERT INTO member (loginId, loginPw, address, roadAddress, jibunAddress, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      [loginId, loginPw, zonecode, roadAddress, jibunAddress, latitude, longitude],
+    );
 
     // 삽입된 행의 ID를 반환
     res.status(200).json({ id: result.insertId });
