2ec50848f6d336fefca68167778e5c33523b7d01
JANG-MINYOUNG <wkdalsdudqq@gmail.com>
2024-05-13 17:06:43 +0900

    안드로이드 설정

--- src/app/sub/CartPage.js
+++ src/app/sub/CartPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Product.css';
 import { IoIosArrowBack } from 'react-icons/io';
-import {  useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const CartPage = () => {
   const [cart, setCart] = useState([]);
@@ -12,6 +12,7 @@ const CartPage = () => {
   const goBack = () => {
     history.goBack();
   };
+
   useEffect(() => {
     const fetchCartItems = async () => {
       setLoading(true);
@@ -42,11 +43,10 @@ const CartPage = () => {
     }
     setTotalPrice(total);
   };
-
   const fetchCartItems = async () => {
     setLoading(true);
     try {
-      const response = await axios.get('/api/cartadd');
+      const response = await axios.get('/api/cart');
       setCart(response.data.cartItems);
       calculateTotalPrice(response.data.cartItems);
       setLoading(false);
@@ -55,87 +55,82 @@ const CartPage = () => {
       setLoading(false);
     }
   };
-  const addToCart = async (productId, product_name, price, quantity, imageURL) => {
-  try {
-    const response = await axios.post('/api/cartadd', {
-      productId: productId,
-      product_name: product_name,
-      price: price,
-      quantity: quantity,
-      imageURL: imageURL
-    });
-    console.log('Item added to cart:', response.data);
-    // 성공적으로 상품이 장바구니에 추가되었을 때 수행할 작업 추가
-  } catch (error) {
-    console.error('Error adding item to cart:', error);
-    // 상품 추가 중 오류가 발생했을 때 수행할 작업 추가
-  }
-};
-
-
   const removeFromCart = async (productId) => {
     try {
+      console.log('Product ID:', productId);
       await axios.delete(`/api/cartdelete?productId=${productId}`);
-      // 장바구니에서 상품을 삭제하고 재로딩하여 변경된 장바구니를 반영
+      // 장바구니 아이템이 삭제된 후에만 장바구니 다시 불러오기
       fetchCartItems();
     } catch (error) {
       console.error('Error removing item from cart:', error);
     }
   };
 
+  const handleSubmitPayment = async () => {
+    try {
+      // 결제 정보를 생성하고
+      // 필요한 정보를 쿼리 문자열로 추가하여 결제 페이지로 이동
+      history.push(`/tosspay/`);
+    } catch (error) {
+      console.error('결제 확인 중 오류 발생:', error);
+      history.push('/fail');
+    }
+  };
+
   return (
-    <><div
-    style={{
-      position: 'fixed',
-      left: '20px',
-      zIndex: '999',
-      width: '100%',
-      background: 'white',
-    }}>
-    <IoIosArrowBack style={{ fontSize: '30px', cursor: 'pointer' }} onClick={goBack} />
-  </div>
-    <div className="cart-container">
-      <div className="cart-content">
-        <div className="cartbar">
-          <h2>장바구니</h2>
-        </div>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <div>
-            {cart.length === 0 ? (
-              <p>장바구니가 비어 있습니다.</p>
-            ) : (
-              <div>
-                {cart.map((item, index) => (
-                  <div key={index} style={{ marginBottom: '20px', display: 'flex' }}>
-                    <img
-                      src={item.imageURL}
-                      alt={item.product_name}
-                      style={{ width: '80px', height: '80px', marginRight: '20px' }}
-                    />
-                    <div>
-                      <p>{item.product_name}</p>
-                      <p>{item.price.toLocaleString()}</p>
-                      <p>수량: {item.quantity}</p>
-                      <button onClick={() => removeFromCart(item.product_id)}>삭제</button>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
+    <>
+      <div
+        style={{
+          position: 'fixed',
+          left: '20px',
+          zIndex: '999',
+          width: '100%',
+          background: 'white',
+        }}>
+        <IoIosArrowBack style={{ fontSize: '30px', cursor: 'pointer' }} onClick={goBack} />
+      </div>
+      <div className="cart-container">
+        <div className="cart-content">
+          <div className="cartbar">
+            <h2>장바구니</h2>
           </div>
-        )}
-      </div>
-      <div className="cart-footer">
-        <div className="cart-actions">
-          <button>결제하기</button>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <div>
+              {cart.length === 0 ? (
+                <p>장바구니가 비어 있습니다.</p>
+              ) : (
+                <div>
+                  {cart.map((item, index) => (
+                    <div key={index} style={{ marginBottom: '20px', display: 'flex' }}>
+                      <img
+                        src={item.imageURL}
+                        alt={item.product_name}
+                        style={{ width: '80px', height: '80px', marginRight: '20px' }}
+                      />
+                      <div>
+                        <p>{item.product_name}</p>
+                        <p>{item.price.toLocaleString()}</p>
+                        <p>수량: {item.quantity}</p>
+                        <button onClick={() => removeFromCart(item.product_id)}>삭제</button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          )}
         </div>
-        <div className="total-price">
-          <p>총 가격: {totalPrice.toLocaleString()}원</p>
+        <div className="cart-footer">
+          <div className="cart-actions">
+            <button onClick={handleSubmitPayment}>결제하기</button>
+          </div>
+          <div className="total-price">
+            <p>총 가격: {totalPrice.toLocaleString()}원</p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
